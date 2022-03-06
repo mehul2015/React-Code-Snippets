6ef54e81b66ed521f377b97d0a2c5b01bc79956e
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-03-07 01:11:53 +0530

    Update SongList.jsx

--- Redux Setup/components/SongList.jsx
+++ Redux Setup/components/SongList.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
-const SongList = (props) => {
-  const listOfSongs = props.songs.map((song) => {
+const SongList = ({ songs, selectSong }) => {
+  const listOfSongs = songs.map((song) => {
     return (
-      <div key={song.title}>
-        <h1>{song.title}</h1>
-        <p>{song.duration}</p>
+      <div key={song.title} className='item'>
+        <div className='right floated content'>
+          <button
+            className='ui button primary'
+            onClick={() => selectSong(song)}
+          >
+            Select
+          </button>
+        </div>
+        <div className='content'>{song.title}</div>
       </div>
     );
   });
 
-  return <>{listOfSongs}</>;
+  return <div className='ui divided list'>{listOfSongs}</div>;
 };
 
 const mapStateToProps = (state) => {
   //Always returns an object (Key : Value Pair)
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong,
+})(SongList);
 
 //the returned object from the mapstatetoprops function is an object with a key value pair
 //the key name is the prop name inside our component
- 
